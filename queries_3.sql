772caf75dbdf44cdac093b20b322218267ec972d
Gil Zalcberg <76707144+gilzalc@users.noreply.github.com>
2023-07-19 15:27:54 +0300

    Update queries_3.sql
    fixing syntax

--- queries_3.sql
+++ queries_3.sql
@@ -1,12 +1,10 @@
---שאלה 1) לכל כנסת החזר את מספר המפלגות השונות בהן כיהנו חברי כנסת באותה הכנסת.
--- יש להחזיר טבלה עם העמודות number,partyCount ממוינת לפי number
+
 SELECT number, COUNT(DISTINCT party) AS partyCount
 FROM memberinknesset
 GROUP BY number
 ORDER BY number;
 
---שאלה 2) לכל כנסת החזר את הגיל הממוצע של חברי הכנסת שכיהנו באותה הכנסת .
--- יש להחזיר טבלה עם העמודות number,avgAge ממוינת לפי number
+
 SELECT number, AVG(k.startyear - m.birthyear) AS partyCount
 FROM memberinknesset mik
          NATURAL JOIN knessets k
@@ -15,10 +13,7 @@ GROUP BY number
 ORDER BY number;
 
 
---שאלה 3)  (נאמר שחבר כנסת הוא מתמיד נאמן, אם כיהן לפחות בחמש כנסות שונות (לאו דווקא ברצף),
--- ובנוסף,כיהן עבור מפלגה אחת בלבד לאורך כל כהונותיו בכנסת.
--- החזר את שמות כל חברי הכנסה תמתמידים-נאמנים.
--- יש להחזיר טבלה עם העמודה name ממוינת לפי name
+
 
 SELECT MAX(name) as name
 FROM members
@@ -28,10 +23,7 @@ HAVING COUNT(DISTINCT party) = 1 AND COUNT(number) >= 5
 ORDER BY MAX(name);
 
 
---שאלה 4) לכל כנסת, החזר את המפלגה הכי גדולה(כלומר שכיהנו מטעמה בכנסת זו הכי הרבה חברי כנסת)
---ואת מספר חברי הכנסת מאותה מפלגה באותה הכנסת. יש להחזיר טבלה עם העמודות number,party,memberCount
--- ממויינת לפי number ומיון שניוני לפי party.
--- אם יש כמה מפלגות גדולות ביותר באותה כנסת, יש להחזיר את כולם, כל אחת בשורה נפרדת.
+
 SELECT k.number, mik.party, COUNT(*) AS memberCount
 FROM knessets k
          NATURAL JOIN memberinknesset mik
@@ -55,11 +47,6 @@ ORDER BY number, party;
 
 
 
---שאלה 5) נאמר שמפלגה היא מקדמת נשים אם יש כנסת בה לפחות %30 מחברי הכנסת באותה מפלגה היו נשים.
---מצאו את כל המפלגות שהיו מקדמות נשים בלפחות כנסת אחת.
---  יש להחזיר את כל הכנסות בהם המפלגה קידמה נשים
---יש להחזיר טבלה עם העמודות party,number,femalePercent ממויין לפי party ואז לפי number.
-
 WITH T1(party, number, gender) AS (SELECT party,
                                           number,
                                           ((CASE
@@ -79,13 +66,7 @@ HAVING 0.3 <= (SUM(gender)) / (COUNT(*))
 ORDER BY party, number;
 
 
---  6) נאמר שהמרחק בין שני חברי כנסת m1 ו- m2 הוא 1 אם שניהם כיהנו באותה הכנסת עבור אותה המפלגה.
--- בהמשך לכך, אם חבר כנסת m3 כיהן באותה הכנסת עבור אותה המפלגה כמו m2 (אך לא עם m1) נגיד שm1 וm3 הם במרחק 2.
---
--- למשל, גאולה כהן כיהנה בכנסת ה-8 יחד עם מנחם בגין במפלגת הליכוד, ולכן היא במרחק 1 ממנו.
--- חנן פורת לא כיהן בכנסת ה-8 ,אבל כן כיהן יחד גם גאולה כהן בכנסת ה-10 במפלגת תחיה לכן הוא במרחק 2 ממנחם בגין.
---כתבו שאילתה רקורסיבית אשר מחזירה את כל חברי הכנסת שהמרחק שלהם ממנחם בגין (Menachem Begin) גדול מ3.
---יש להחזיר טבלה עם עמודה אחת ממוינת בשם name ובה שמות חברי הכנסת.
+
 WITH RECURSIVE T1(uid, party, number, dist) AS
                    (SELECT M.uid, MIK.party, MIK.number, 0 AS dist
                     FROM members M
